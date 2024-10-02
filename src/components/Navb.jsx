45d16a1b2e92fbc4112602f3dbeec1a8ac544064
David Udoh <udohdavid004@gmail.com>
2024-10-02 06:43:28 +0100

    debug2

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Logo from "./Logo";
 
 function Navb({ children }) {
