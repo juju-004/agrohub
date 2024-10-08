d3efb4fc797bfdb3ba1d4a3a3889323b95b664ca
David Udoh <udohdavid004@gmail.com>
2024-10-08 11:45:46 +0100

    debug3

--- src/components/CourseSec.jsx
+++ src/components/CourseSec.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Header from "./Header";
 import Image from "next/image";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+"use client";
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Header({ children }) {
   return (
-    <h1 className="mt-20 text-6xl wp mb-7 mx-auto text-center relative">
-      {children}
-      <span className="bg-cmedium w-10 h-2 rounded-2xl absolute top-full"></span>
-    </h1>
+    <Container className="w-screen fx overflow-hidden mb-7 mt-20">
+      <h4 className="sm:text-5xl text-3xl wp relative text-center">
+        {children}
+        <span className="bg-cmedium w-10 h-2 rounded-2xl absolute top-full"></span>
+      </h4>
+    </Container>
   );
 }
 
